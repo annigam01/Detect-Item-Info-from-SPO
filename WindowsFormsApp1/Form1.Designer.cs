d45136e06b3d00c24958e8e8c178d29670fda15a
Anand Nigam <annigam01@hotmail.com>
2017-05-09 16:21:44 +0530

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(163, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1125, 31);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "https://mstnd550949.sharepoint.com/sites/contoso/Documents/20%20Years%20of%20Rese" +
+    "arch.docx";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Site Raw URL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Password";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(163, 93);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(1125, 31);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "admin@MSTND550949.onmicrosoft.com";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(163, 139);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(1125, 31);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "ms!Demos365";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(163, 248);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(251, 53);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Execute";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(163, 376);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(1125, 318);
+            this.textBox4.TabIndex = 7;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(183, 194);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(337, 31);
+            this.textBox5.TabIndex = 8;
+            this.textBox5.Text = "sites";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(152, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Managed Path";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1318, 752);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.SharePoint.Client;
+using System.Security;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        public static bool isRoot = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sitecoll = DetectSiteCollectionURL(textBox1.Text);
+            string list = DetectListNameURL(sitecoll, textBox1.Text);
+            string FileName = DetectFileName(textBox1.Text);
+
+            MessageBox.Show(sitecoll);
+            MessageBox.Show(list);
+            MessageBox.Show(FileName);
+
+            connectTOSP(sitecoll, list, FileName);
+        }
+
+        private string DetectFileName(string url)
+        {
+            string[] test = url.Split('/');
+
+            return System.Web.HttpUtility.UrlDecode(test[test.Length - 1]);
+
+        }
+        private string DetectSiteCollectionURL(string url)
+        {
+
+            if (url.ToLower().Contains(textBox5.Text.ToLower()))
+            {
+                //contains managed path
+                isRoot = false;
+                return DetectNonRootSiteCollectionURL(url);
+
+            }
+            else
+            {
+                //contains non-root site collection
+                isRoot = true;
+                return DetectRootSiteCollectionURL(url);
+
+            }
+
+        }
+        private string DetectListNameURL(string URL, string rawFullURL)
+        {
+            if (isRoot)
+            {
+                return DetectListNameForRootFromURL(URL, rawFullURL);
+            }
+            else
+            {
+                return DetectListNameForNonRootFromURL(URL, rawFullURL);
+            }
+
+
+
+        }
+        private string DetectNonRootSiteCollectionURL(string rawURL)
+        {
+
+            //only meant for non-root site url
+
+            //remove all the special char to normal chars
+            string _rawurl = System.Web.HttpUtility.UrlDecode(rawURL);
+
+            //convert string to url
+            Uri _uri = new Uri(_rawurl);
+
+            //get the http part
+            string scheme = _uri.Scheme;
+
+            //get the server name
+            string server = _uri.Host;
+
+            //get the managed path 'SITES'
+            string managedpath = textBox5.Text;
+
+            //get the sitecollection name
+            string sitecoll = (_uri.Segments)[2].ToString();
+
+            //final url for site collection
+            string FullSiteCollURL = $"{scheme}://{server}/{managedpath}/{sitecoll}";
+
+            ////get the library name
+            //string lib = (_rawurl.Substring(FullSiteCollURL.Length).Split('/')[0]);
+
+            return FullSiteCollURL;
+
+        }
+        private string DetectRootSiteCollectionURL(string rawURL)
+        {
+
+            //only meant for rootsite collection
+
+            //remove all the special char to normal chars
+            string _rawurl = System.Web.HttpUtility.UrlDecode(rawURL);
+
+            //convert string to url
+            Uri _uri = new Uri(_rawurl);
+
+            //get the http part
+            string scheme = _uri.Scheme;
+
+            //get the server name
+            string server = _uri.Host;
+
+
+            //get the sitecollection name
+            string sitecoll = (_uri.Segments)[2].ToString();
+
+            //final url for site collection
+            string FullSiteCollURL = $"{scheme}://{server}";
+
+            return FullSiteCollURL;
+
+        }
+        private string DetectListNameForRootFromURL(string SiteCollectionURL, string rawFullURL)
+        {
+            //get the library name
+            return (rawFullURL.Substring(SiteCollectionURL.Length).Split('/')[1]);
+
+        }
+        private string DetectListNameForNonRootFromURL(string SiteCollectionURL, string rawFullURL)
+        {
+            //get the library name
+            return (rawFullURL.Substring(SiteCollectionURL.Length).Split('/')[0]);
+
+        }
+        private void connectTOSP(string siteurl, string listname, string filename)
+        {
+            MessageBox.Show(DetectList(siteurl, listname, filename));
+            
+        }
+
+        private string DetectList(string siteurl, string listname, string filename)
+        {
+            string status = "";
+            try
+            {
+                //try if its a list
+                status= TryGettingList(siteurl, listname, filename);
+            }
+            catch (Exception)
+            {
+
+                //try if its a library
+                status= TryGettingLibrary(siteurl, listname, filename);
+            }
+
+            return status;
+
+        }
+
+        private string TryGettingList(string siteurl, string listname, string filename)
+        {
+            string status = "";
+            using (ClientContext ctx = new ClientContext(siteurl))
+            {
+                ctx.Credentials = new SharePointOnlineCredentials(textBox2.Text, ConvertToSecureString(textBox3.Text));
+
+                ListCollection AllList = ctx.Web.Lists;
+                List Mylist = ctx.Web.GetList(listname);
+                ctx.Load(Mylist);
+
+                ctx.ExecuteQuery();
+
+
+                status = $"List {Mylist.Title}{Environment.NewLine}";
+                
+            }
+            return status;
+        }
+        
+        private string TryGettingLibrary(string siteurl, string listname, string filename)
+        {
+            string status = "";
+            using (ClientContext ctx = new ClientContext(siteurl))
+                {
+                    ctx.Credentials = new SharePointOnlineCredentials(textBox2.Text, ConvertToSecureString(textBox3.Text));
+
+                    ListCollection AllList = ctx.Web.Lists;
+                    ctx.Load(AllList, lst => lst.Include(l => l.Title,
+                                                         l => l.DefaultViewUrl)
+                                                 .Where(l => l.IsSystemList != true));
+
+
+                    ctx.Load(AllList);
+
+                    ctx.ExecuteQuery();
+
+                    foreach (List lst in AllList)
+                    {
+                        if (lst.DefaultViewUrl.ToString().Contains(listname))
+                        {
+                            status =  $"Document library -{lst.Title}{Environment.NewLine}";
+                        }
+
+                    }
+                    
+                }
+            return status;
+
+        }
+        private static SecureString ConvertToSecureString(string strPassword)
+        {
+            var secureStr = new SecureString();
+            if (strPassword.Length > 0)
+            {
+                foreach (var c in strPassword.ToCharArray()) secureStr.AppendChar(c);
+            }
+            return secureStr;
+
+        }
+    }
+}
