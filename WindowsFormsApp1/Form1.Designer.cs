3e67070fbbc61e3813f05a9566ee75edb0887551
Anand Nigam <annigam01@hotmail.com>
2017-05-12 14:49:34 +0530

    this version works for normal document library & also master page gallery, solution gallery

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -38,16 +38,21 @@
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(163, 46);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(1125, 31);
+            this.textBox1.Size = new System.Drawing.Size(498, 31);
             this.textBox1.TabIndex = 0;
-            this.textBox1.Text = "https://mstnd550949.sharepoint.com/sites/contoso/Documents/20%20Years%20of%20Rese" +
-    "arch.docx";
+            this.textBox1.Text = "https://mstnd550949.sharepoint.com/sites/contoso/_catalogs/wp/BusinessDataFilter." +
+    "dwp";
             // 
             // label1
             // 
@@ -104,10 +109,10 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(163, 376);
+            this.textBox4.Location = new System.Drawing.Point(163, 331);
             this.textBox4.Multiline = true;
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(1125, 318);
+            this.textBox4.Size = new System.Drawing.Size(1125, 467);
             this.textBox4.TabIndex = 7;
             // 
             // textBox5
@@ -128,11 +133,48 @@
             this.label4.TabIndex = 9;
             this.label4.Text = "Managed Path";
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(579, 195);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(230, 29);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "SharePoint Online?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(682, 35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(208, 47);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Open CSV";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(934, 35);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(208, 47);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Save Output CSV";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1318, 752);
+            this.ClientSize = new System.Drawing.Size(1445, 834);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBox4);
@@ -162,6 +204,11 @@
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -9,12 +9,26 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Microsoft.SharePoint.Client;
 using System.Security;
+using System.Net;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : System.Windows.Forms.Form
     {
         public static bool isRoot = false;
+        public static ClientContext GlobalCtx = null;
+        public static bool isSPO = true;
+        public static bool isSpecialFolders = false;
+        public string list = "";
+        public string FileName = "";
+        public int SpecialFolderType = 0;
+        public string CSVFileLocation = "";
+        public string[] AllURL = null;
+        public string GlobalManagedPath ="";
+        public string OutputFileLocation = "";
+
+        public enum SpecialFolderTypeEnum {webpart,masterpage,listtemplate,solution};
+
         public Form1()
         {
             InitializeComponent();
@@ -22,15 +36,33 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string sitecoll = DetectSiteCollectionURL(textBox1.Text);
-            string list = DetectListNameURL(sitecoll, textBox1.Text);
-            string FileName = DetectFileName(textBox1.Text);
+            foreach (string url in AllURL)
+            {
+                LogtoFile(url + ";");
+                DoWork(url);
+            }
+                      
+        }
 
-            MessageBox.Show(sitecoll);
-            MessageBox.Show(list);
-            MessageBox.Show(FileName);
+        private void DoWork(string url)
+        {
+            GlobalManagedPath = textBox5.Text;
+            string sitecoll = DetectSiteCollectionURL(url);
+            isSPO = checkBox1.Checked;
+            
 
-            connectTOSP(sitecoll, list, FileName);
+            if (!isSpecialFolders)
+            {
+                list = System.Web.HttpUtility.UrlDecode(DetectListNameURL(sitecoll, url));
+                FileName = DetectFileName(url);
+                LogtoFile(connectTOSP(sitecoll, list, FileName));
+            }
+
+        }
+
+        private void LogtoFile(string msg)
+        {
+            System.IO.File.AppendAllText(OutputFileLocation,msg);
         }
 
         private string DetectFileName(string url)
@@ -40,25 +72,146 @@ namespace WindowsFormsApp1
             return System.Web.HttpUtility.UrlDecode(test[test.Length - 1]);
 
         }
+
         private string DetectSiteCollectionURL(string url)
         {
-
-            if (url.ToLower().Contains(textBox5.Text.ToLower()))
+            if (url.ToLower().Contains("_catalogs"))
+            {
+                isSpecialFolders = true;
+                
+                //masterpages and other special folders
+                return DetectSpecialFolderFiles(url, DetectFileName(url));
+            }
+            else if (url.ToLower().Contains(textBox5.Text.ToLower()))
             {
                 //contains managed path
+                isSpecialFolders = false;
                 isRoot = false;
                 return DetectNonRootSiteCollectionURL(url);
 
             }
-            else
+            else 
             {
                 //contains non-root site collection
                 isRoot = true;
+                isSpecialFolders = false;
                 return DetectRootSiteCollectionURL(url);
 
             }
 
         }
+
+        private string DetectSpecialFolderFiles(string url,string filename)
+        {
+            Uri SiteCollUri = new Uri(url);
+
+            //eg - csv gives you this - https://mstnd550949.sharepoint.com/sites/contoso/_catalogs/wp/BusinessDataFilter.dwp
+
+            //the below gives you this - https://mstnd550949.sharepoint.com/
+            string baseurl = $"{SiteCollUri.Scheme}://{SiteCollUri.Host}/{GlobalManagedPath}";
+
+            // the below gives you - sites/contoso/_catalogs/wp/BusinessDataFilter.dwp
+            string[] temp = url.Substring(baseurl.Length).Split('/');
+            string sitecollname = temp[1];
+            string sitecollurl = $"{baseurl}/{sitecollname}/";
+
+            
+
+
+            if (url.ToLower().Contains("/_catalogs/wp")) // this is webpart reference
+            {
+                
+                
+                handleSpecialFolder(SpecialFolderTypeEnum.webpart,sitecollurl,filename);
+
+            }
+            else if (url.ToLower().Contains("/_catalogs/lt")) // this is list template gallary
+            {
+                
+                handleSpecialFolder(SpecialFolderTypeEnum.listtemplate, sitecollurl, filename);
+            }
+            else if (url.ToLower().Contains("/_catalogs/masterpage")) //this is master page
+            {
+                handleSpecialFolder(SpecialFolderTypeEnum.masterpage, sitecollurl, filename);
+            }
+            else if (url.ToLower().Contains("/_catalogs/solution")) // this is solutions gallery
+            {
+                handleSpecialFolder(SpecialFolderTypeEnum.solution,sitecollurl, filename);
+            }
+
+            return "";
+            
+        }
+
+        private void handleSpecialFolder(SpecialFolderTypeEnum SPType,string url,string filename)
+        {
+            if (SpecialFolderTypeEnum.webpart == SPType)
+            {
+                GetItemsOwnerForSpecialFolder(url, "Web Part Gallery", filename);
+
+            }
+            else if (SPType == SpecialFolderTypeEnum.listtemplate)
+            {
+                GetItemsOwnerForSpecialFolder(url, "List Template Gallery", filename);
+
+            }
+            else if (SPType == SpecialFolderTypeEnum.masterpage)
+            {
+                GetItemsOwnerForSpecialFolder(url, "Master Page Gallery", filename);
+            }
+            else if (SPType == SpecialFolderTypeEnum.solution)
+            {
+                GetItemsOwnerForSpecialFolder(url, "Solution Gallery", filename);
+            }
+            else {
+                MessageBox.Show("cannot detect the special folder type");
+            }
+        }
+
+        private string GetItemsOwnerForSpecialFolder(string url, string libraryName, string filename)
+        {
+            string listname = "";
+
+            using (ClientContext ctx = new ClientContext(url))
+            {
+                if (isSPO)
+                {
+                    ctx.Credentials = new SharePointOnlineCredentials(textBox2.Text, ConvertToSecureString(textBox3.Text));
+                }
+                else
+                {
+                    NetworkCredential _myCredentials = new NetworkCredential($"{textBox2.Text}", $"{textBox3.Text}");
+                    ctx.Credentials = _myCredentials;
+
+                }
+
+                ctx.Load(ctx.Web.Lists, l => l.Include(lst => lst.Title).Where(lst => lst.Title == libraryName));
+
+                ctx.ExecuteQuery();
+
+                List list = null;
+                Web web = null;
+                ListItemCollection AllItems = null;
+
+                foreach (List l in ctx.Web.Lists)
+                {
+                    web = ctx.Web;
+                    list = l;
+                    var q = new CamlQuery() { ViewXml = $"<View><Query><Where><Eq><FieldRef Name='LinkFilename' /><Value Type='Computed'>{filename}</Value></Eq></Where></Query><ViewFields><FieldRef Name='Editor' /><FieldRef Name='Author' /></ViewFields><QueryOptions /></View>" };
+                    AllItems = list.GetItems(q);
+                    ctx.Load(AllItems);
+                    ctx.ExecuteQuery();
+                }
+                foreach (var item in AllItems)
+                {
+                    FieldUserValue oValue = item["Author"] as FieldUserValue;
+                    MessageBox.Show(oValue.LookupValue);
+                    LogtoFile(oValue.LookupValue + Environment.NewLine);
+                }
+            }
+            return listname;
+        }
+
         private string DetectListNameURL(string URL, string rawFullURL)
         {
             if (isRoot)
@@ -73,6 +226,7 @@ namespace WindowsFormsApp1
 
 
         }
+
         private string DetectNonRootSiteCollectionURL(string rawURL)
         {
 
@@ -92,6 +246,7 @@ namespace WindowsFormsApp1
 
             //get the managed path 'SITES'
             string managedpath = textBox5.Text;
+            GlobalManagedPath = managedpath;
 
             //get the sitecollection name
             string sitecoll = (_uri.Segments)[2].ToString();
@@ -105,6 +260,7 @@ namespace WindowsFormsApp1
             return FullSiteCollURL;
 
         }
+
         private string DetectRootSiteCollectionURL(string rawURL)
         {
 
@@ -132,93 +288,155 @@ namespace WindowsFormsApp1
             return FullSiteCollURL;
 
         }
+
         private string DetectListNameForRootFromURL(string SiteCollectionURL, string rawFullURL)
         {
             //get the library name
             return (rawFullURL.Substring(SiteCollectionURL.Length).Split('/')[1]);
 
         }
+
         private string DetectListNameForNonRootFromURL(string SiteCollectionURL, string rawFullURL)
         {
             //get the library name
             return (rawFullURL.Substring(SiteCollectionURL.Length).Split('/')[0]);
 
         }
-        private void connectTOSP(string siteurl, string listname, string filename)
+
+        private string connectTOSP(string siteurl, string listname, string filename)
         {
-            MessageBox.Show(DetectList(siteurl, listname, filename));
+          return GetItemsOwner(siteurl,DetectList(siteurl, listname), filename);
             
         }
 
-        private string DetectList(string siteurl, string listname, string filename)
+        private List DetectList(string siteurl, string listname)
         {
-            string status = "";
+            List FinalList = null;
             try
             {
                 //try if its a list
-                status= TryGettingList(siteurl, listname, filename);
+                //MessageBox.Show("trying list");
+                FinalList = TryGettingList(siteurl, listname);
+                
             }
             catch (Exception)
             {
 
                 //try if its a library
-                status= TryGettingLibrary(siteurl, listname, filename);
+                //MessageBox.Show("trying Library");
+                FinalList = TryGettingLibrary(siteurl, listname);
             }
 
-            return status;
+            return FinalList;
 
         }
 
-        private string TryGettingList(string siteurl, string listname, string filename)
+        private List TryGettingList(string siteurl, string listname)
         {
-            string status = "";
+            List myList = null;
             using (ClientContext ctx = new ClientContext(siteurl))
             {
-                ctx.Credentials = new SharePointOnlineCredentials(textBox2.Text, ConvertToSecureString(textBox3.Text));
+                if (isSPO)
+                {
+                    ctx.Credentials = new SharePointOnlineCredentials(textBox2.Text, ConvertToSecureString(textBox3.Text));
+                }
+                else
+                {
+                    NetworkCredential _myCredentials = new NetworkCredential($"{textBox2.Text}",$"{textBox3.Text}");
+                    ctx.Credentials = _myCredentials;
 
+                }
+                GlobalCtx = ctx;
                 ListCollection AllList = ctx.Web.Lists;
                 List Mylist = ctx.Web.GetList(listname);
                 ctx.Load(Mylist);
 
                 ctx.ExecuteQuery();
-
-
-                status = $"List {Mylist.Title}{Environment.NewLine}";
+                
+                myList = Mylist;
                 
             }
-            return status;
+            return myList;
         }
-        
-        private string TryGettingLibrary(string siteurl, string listname, string filename)
+
+        private string GetItemsOwner(string siteurl,List _list,string filename)
         {
-            string status = "";
+            string strAuthorName = "";
+            using (ClientContext ctx = GlobalCtx)
+            {
+                Web web = ctx.Web;
+                ctx.Load(_list);
+                ctx.ExecuteQuery();
+                List list = _list;
+
+                var q = new CamlQuery() { ViewXml = $"<View><Query><Where><Eq><FieldRef Name='LinkFilename' /><Value Type='Computed'>{filename}</Value></Eq></Where></Query><ViewFields><FieldRef Name='Editor' /><FieldRef Name='Author' /></ViewFields><QueryOptions /></View>" };
+
+                var r = list.GetItems(q);
+                ctx.Load(r);
+                ctx.ExecuteQuery();
+
+                foreach (var item in r)
+                {
+                    FieldUserValue oValue = item["Author"] as FieldUserValue;
+                    strAuthorName = oValue.LookupValue;
+                    //LogtoFile(strAuthorName + Environment.NewLine);
+                    
+                }
+
+
+            }
+
+            return strAuthorName;
+        }
+
+        private List TryGettingLibrary(string siteurl, string listname)
+        {
+            List MyList = null;
+            try
+            {
+
             using (ClientContext ctx = new ClientContext(siteurl))
                 {
+                if (isSPO)
+                {
                     ctx.Credentials = new SharePointOnlineCredentials(textBox2.Text, ConvertToSecureString(textBox3.Text));
+                }
+                else
+                {
+                    NetworkCredential _myCredentials = new NetworkCredential($"{textBox2.Text}", $"{textBox3.Text}");
+                    ctx.Credentials = _myCredentials;
+
+                }
+                GlobalCtx = ctx;
 
                     ListCollection AllList = ctx.Web.Lists;
-                    ctx.Load(AllList, lst => lst.Include(l => l.Title,
-                                                         l => l.DefaultViewUrl)
-                                                 .Where(l => l.IsSystemList != true));
-
-
+                    ctx.Load(AllList, lst => lst.Include(l => l.Title, l=>l.DefaultViewUrl));
+                
                     ctx.Load(AllList);
 
                     ctx.ExecuteQuery();
 
                     foreach (List lst in AllList)
                     {
-                        if (lst.DefaultViewUrl.ToString().Contains(listname))
-                        {
-                            status =  $"Document library -{lst.Title}{Environment.NewLine}";
-                        }
-
+                      
+                        if (listname.ToLower().ToString() == (lst.DefaultViewUrl.Split('/'))[3].ToLower().ToString())
+                    {
+                        //MessageBox.Show("match");
+                        MyList = lst;
+                    }
+                    
                     }
                     
                 }
-            return status;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception trying at library " +ex.Message);
+            }
+            return MyList;
 
         }
+
         private static SecureString ConvertToSecureString(string strPassword)
         {
             var secureStr = new SecureString();
@@ -228,6 +446,38 @@ namespace WindowsFormsApp1
             }
             return secureStr;
 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    AllURL = System.IO.File.ReadAllLines(openFileDialog1.FileName);
+
+                    foreach (string item in AllURL)
+                    {
+                        textBox4.Text += item + Environment.NewLine;
+                    }
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+                OutputFileLocation = saveFileDialog1.FileName;
+            }
         }
     }
 }
